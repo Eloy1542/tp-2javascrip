fd9448653cc7cc97969733baa48fa13c2a5628f1
Eloy Pintos <eloy_pintos14@hotmail.com>
2022-07-16 14:29:42 -0300

    ejercicio 10 resuelto completo

--- js/ej10.js
+++ js/ej10.js
@@ -1,6 +1,6 @@
 let columnas = parseInt(prompt("Ingrese un numero de columnas"));
 let filas = parseInt(prompt("Ingrese un numero de filas"));
-
+let resultado = filas * columnas
 document.write(`<table>
 <tbody>`);
 
@@ -8,7 +8,8 @@ for (let indicefila = 0; indicefila < filas; indicefila++) {
   document.write(`<tr>`);
 
   for (let indicecolumna = 0; indicecolumna < columnas; indicecolumna++) {
-    document.write(`<td>hola</td>`);
+    document.write(`<td> ${resultado} </td>`);
+    resultado--
   }
   document.write(`</tr>`);
 }
