56fc665566c5c30a2b52a5c37951f33b2607d2fa
Eloy Pintos <eloy_pintos14@hotmail.com>
2022-07-18 13:48:12 -0300

    arreglo ejercicio 14

--- js/ej14.js
+++ js/ej14.js
@@ -1,5 +1,12 @@
-let frase = prompt("Introduce una frase");
-
-let cadena = frase.split("");
-
-document.write(cadena)
+let cadena = prompt("Introduce una cadena de texto:");
+let caracteres = cadena.length;
+let caracter;
+let i=0;
+for (i = 0; i < caracteres; i++) {
+  caracter = cadena.charAt(i); 
+  if (i == caracteres - 1) {
+    document.write(caracter);
+  } else {
+    document.write(caracter + "-");
+  }
+}
