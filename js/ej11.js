d17073d6669459333290fcbef80d14aaea0fe485
Eloy Pintos <eloy_pintos14@hotmail.com>
2022-07-17 23:12:36 -0300

    avanzando ejercicio 11 y 17

--- js/ej11.js
+++ js/ej11.js
@@ -1,3 +1,8 @@
-let nombres = prompt("Introduce tu nombre");
-let edades = parseInt(prompt("Introduce tu edad"));
+let nombres1 = prompt("Introduce tu nombre") && parseInt(prompt("Introduce tu edad"));
+let nombres2 = prompt("Introduce tu nombre") && parseInt(prompt("Introduce tu edad"));
+let nombres3 = prompt("Introduce tu nombre") && parseInt(prompt("Introduce tu edad"));
 
+
+let mayor = Math.max(nombre1,nombre2,nombres3);
+
+document.write(mayor);
