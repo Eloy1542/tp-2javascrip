dbca3b49964f74e6d18188d69178942e2f708605
Eloy Pintos <eloy_pintos14@hotmail.com>
2022-07-18 13:01:36 -0300

    ejercicio 9 ya pide los multilplos de 4 y 9

--- js/ej8.js
+++ js/ej8.js
@@ -5,7 +5,7 @@ let y = 0
 if(numero > 1 && numero <= 50){
     for(x=1; x<=numero ; x++){
         for(y=0 ; y < x ; (y++)){
-            document.write(x)
+            document.write(y)
         }
         document.write("<br>")
     }
--- js/ej9.js
+++ js/ej9.js
@@ -1,8 +1,15 @@
 let i = 0
 
 for(i=0; i<=500; i++){
+    if(i%4 ===0){
+        document.write(i + " Es multiplo de 4")
+    }
+    if(i%9 ===0){
+        document.write(i +" Es multiplo de 9 ")
+    }
     if(i%5 ===0 ){
         document.write("---------"+ "<br>")
     }
-    document.write(i+ "<br>")
+    document.write("<br>")
 }
+
